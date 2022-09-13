d5618d53a0188912a3f53285743625d196cd8763
Bhannasa <adityabhan125@gmail.com>
2022-09-13 07:59:50 +0530

    frontend at docs added

--- popup.js
+++ popup.js
@@ -1,27 +1,28 @@
-document.getElementById('changeColor').onclick = () => {
-    document.body.style.backgroundColor = "yellow"
-}
 (async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     console.log(tab.url)
     chrome.storage.sync.get("links", ({ links }) => {
         let def = "Succesfully added";
+        let title = tab.title;
+        let url = tab.url;
         if(!links)  links = [];
-        if(links.find(link => tab.url === link) !== undefined){
+        if(links.find(link => url === link.url) !== undefined){
             def = "Already added."
         }else{
-            links.push(tab.url);
+            links.push({url, done: false, title});
             chrome.storage.sync.set({links})
         }
+        document.getElementById('text').innerText = title
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             func: runOnWindow,
-            args: [def],
+            args: [def, title],
         });
     });
 })();
-function runOnWindow(def) {
+function runOnWindow(def, title) {
     console.log(def)
+    console.log(title)
     chrome.storage.sync.get("links",({links})=>{
         console.log(links)
     })
